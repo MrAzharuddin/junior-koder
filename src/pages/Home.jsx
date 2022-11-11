24ab3cc66252b61c46a1feae9a9125cdaf0a1e1f
MrAzharuddin <42718729+MrAzharuddin@users.noreply.github.com>
2022-11-11 17:28:13 +0530

    1 file modified

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -330,6 +330,27 @@ export default function Home() {
                     'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.',
                   ],
                 },
+                {
+                  title: 'Accordion 4',
+                  paras: [
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.',
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.',
+                  ],
+                },
+                {
+                  title: 'Accordion 5',
+                  paras: [
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.',
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.',
+                  ],
+                },
+                {
+                  title: 'Accordion 6',
+                  paras: [
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.',
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.',
+                  ],
+                },
               ].map((item, idx) => (
                 <AccordionItem key={idx}>
                   <AccordionItemHeading className="bg-primary px-6 py-2">
