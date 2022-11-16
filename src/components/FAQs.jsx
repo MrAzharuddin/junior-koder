fd1b4f94e799352715493771dacf8ace834ff6e3
MrAzharuddin <42718729+MrAzharuddin@users.noreply.github.com>
2022-11-16 23:40:57 +0530

    1 file modified

--- src/components/FAQs.jsx
+++ src/components/FAQs.jsx
@@ -1,16 +1,16 @@
-import { useState, Fragment } from "react";
+import { useState, Fragment } from 'react';
 import {
   Accordion,
   AccordionHeader,
   AccordionBody,
-} from "@material-tailwind/react";
+} from '@material-tailwind/react';
 
 function Icon({ id, open }) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       className={`${
-        id === open ? "rotate-180" : ""
+        id === open ? 'rotate-180' : ''
       } h-5 w-5 transition-transform`}
       fill="none"
       viewBox="0 0 24 24"
@@ -23,51 +23,74 @@ function Icon({ id, open }) {
 }
 export default function FAQs() {
   const [open, setOpen] = useState(0);
- 
+
   const handleOpen = (value) => {
     setOpen(open === value ? 0 : value);
   };
- 
+
   const customAnimation = {
     mount: { scale: 1 },
     unmount: { scale: 0.7 },
   };
- 
+
   return (
     <Fragment>
-      <Accordion open={open === 1} animate={customAnimation} icon={<Icon id={1} open={open} />}>
-        <AccordionHeader onClick={() => handleOpen(1)} >
-          What is Material Tailwind?
-        </AccordionHeader>
-        <AccordionBody>
-          We&apos;re not always in the position that we want to be at.
-          We&apos;re constantly growing. We&apos;re constantly making mistakes.
-          We&apos;re constantly trying to express ourselves and actualize our
-          dreams.
-        </AccordionBody>
-      </Accordion>
-      <Accordion open={open === 2} animate={customAnimation}>
-        <AccordionHeader onClick={() => handleOpen(2)}>
-          How to use Material Tailwind?
-        </AccordionHeader>
-        <AccordionBody>
-          We&apos;re not always in the position that we want to be at.
-          We&apos;re constantly growing. We&apos;re constantly making mistakes.
-          We&apos;re constantly trying to express ourselves and actualize our
-          dreams.
-        </AccordionBody>
-      </Accordion>
-      <Accordion open={open === 3} animate={customAnimation}>
-        <AccordionHeader onClick={() => handleOpen(3)}>
-          What can I do with Material Tailwind?
-        </AccordionHeader>
-        <AccordionBody>
-          We&apos;re not always in the position that we want to be at.
-          We&apos;re constantly growing. We&apos;re constantly making mistakes.
-          We&apos;re constantly trying to express ourselves and actualize our
-          dreams.
-        </AccordionBody>
-      </Accordion>
+      {[
+        {
+          title: 'Accordion 1',
+          paras: [
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.',
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.',
+          ],
+        },
+        {
+          title: 'Accordion 2',
+          paras: [
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.',
+          ],
+        },
+        {
+          title: 'Accordion 3',
+          paras: [
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.',
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.',
+          ],
+        },
+        {
+          title: 'Accordion 4',
+          paras: [
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.',
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.',
+          ],
+        },
+        {
+          title: 'Accordion 5',
+          paras: [
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.',
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.',
+          ],
+        },
+        {
+          title: 'Accordion 6',
+          paras: [
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.',
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.',
+          ],
+        },
+      ].map(({ title, paras }, index) => {
+        return (
+          <Accordion
+            open={open === index}
+            animate={customAnimation}
+            icon={<Icon id={index} open={open} />}
+          >
+            <AccordionHeader onClick={() => handleOpen(index)}>
+              {title}
+            </AccordionHeader>
+            <AccordionBody>{paras}</AccordionBody>
+          </Accordion>
+        );
+      })}
     </Fragment>
   );
 }
