2c3e569fbb7e75426af7642d5e30ed8467340862
MrAzharuddin <42718729+MrAzharuddin@users.noreply.github.com>
2022-11-16 23:16:59 +0530

    4 files modified

--- src/components/FAQs.jsx
+++ src/components/FAQs.jsx
@@ -1,7 +1,57 @@
-export default function FAQs(){
-  return(
-    <div>
-      <h1>Hello Faqs</h1>
-    </div>
-  )
+import { useState, Fragment } from "react";
+import {
+  Accordion,
+  AccordionHeader,
+  AccordionBody,
+} from "@material-tailwind/react";
+ 
+export default function FAQs() {
+  const [open, setOpen] = useState(0);
+ 
+  const handleOpen = (value) => {
+    setOpen(open === value ? 0 : value);
+  };
+ 
+  const customAnimation = {
+    mount: { scale: 1 },
+    unmount: { scale: 0.9 },
+  };
+ 
+  return (
+    <Fragment>
+      <Accordion open={open === 1} animate={customAnimation}>
+        <AccordionHeader onClick={() => handleOpen(1)}>
+          What is Material Tailwind?
+        </AccordionHeader>
+        <AccordionBody>
+          We&apos;re not always in the position that we want to be at.
+          We&apos;re constantly growing. We&apos;re constantly making mistakes.
+          We&apos;re constantly trying to express ourselves and actualize our
+          dreams.
+        </AccordionBody>
+      </Accordion>
+      <Accordion open={open === 2} animate={customAnimation}>
+        <AccordionHeader onClick={() => handleOpen(2)}>
+          How to use Material Tailwind?
+        </AccordionHeader>
+        <AccordionBody>
+          We&apos;re not always in the position that we want to be at.
+          We&apos;re constantly growing. We&apos;re constantly making mistakes.
+          We&apos;re constantly trying to express ourselves and actualize our
+          dreams.
+        </AccordionBody>
+      </Accordion>
+      <Accordion open={open === 3} animate={customAnimation}>
+        <AccordionHeader onClick={() => handleOpen(3)}>
+          What can I do with Material Tailwind?
+        </AccordionHeader>
+        <AccordionBody>
+          We&apos;re not always in the position that we want to be at.
+          We&apos;re constantly growing. We&apos;re constantly making mistakes.
+          We&apos;re constantly trying to express ourselves and actualize our
+          dreams.
+        </AccordionBody>
+      </Accordion>
+    </Fragment>
+  );
 }
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,10 @@
+import FAQs from "../components/FAQs";
+
 export default function About() {
   return (
     <div className="min-h-[calc(100vh-12vh)] flex items-center justify-center">
       <h1>Hello About</h1>
+      <FAQs/>
     </div>
   );
 }
