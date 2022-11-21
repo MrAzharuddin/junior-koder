8a3cd3a07535d3ddcc4dd5ce29ced004512031fb
MrAzharuddin <42718729+MrAzharuddin@users.noreply.github.com>
2022-11-21 19:53:14 +0530

    2 files added

---
+++ src/components/OwnTab.jsx
@@ -0,0 +1,64 @@
+import { useState, useEffect } from 'react';
+import { tabsData, homeMeta } from '../data/data';
+
+export default function Tabs() {
+  const [choice, setChoice] = useState('001');
+
+  return (
+    <>
+      <div className="flex justify-center ">
+        <div className="space-y-6">
+          <div className="flex justify-center space-x-8 ">
+            {tabsData.map((value) => {
+              return (
+                <button
+                  key={value.id}
+                  className={`px-4 py-1 rounded-md transition-all duration-700 ease-in-out delay-50 ${
+                    value.id == choice
+                      ? 'bg-blue-400'
+                      : 'bg-transparent opacity-50'
+                  } `}
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setChoice(value.id);
+                  }}
+                >
+                  {value.buttonText}
+                </button>
+              );
+            })}
+          </div>
+          <div className="flex space-x-8">
+            {homeMeta[choice].map((value, index) => {
+              return (
+                <div
+                  key={index}
+                  className={`${
+                    choice == '001'
+                      ? 'opacity-100'
+                      : choice == '002'
+                      ? 'bg-orange-400'
+                      : choice == '003'
+                      ? 'opacity-100'
+                      : 'opacity-0'
+                  } transition-all duration-700 py-10 px-4`}
+                >
+                  <img
+                    className="p-2"
+                    src="https://thurrott.s3.amazonaws.com/2016/03/350x150.gif"
+                    alt="dummy"
+                  />
+                  <h2>{value.name}</h2>
+                  <p>Total Duration : {value.duration}</p>
+                  <p>No of classes : {value.classes}</p>
+                  <p>No of projects : {value.projects}</p>
+                  <button>Explore Now</button>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ src/data/data.js
@@ -0,0 +1,86 @@
+export const tabsData = [
+  {
+    id: '001',
+    buttonText: 'classes 1 - 4',
+  },
+  {
+    id: '002',
+    buttonText: 'classes 5 - 8',
+  },
+  {
+    id: '003',
+    buttonText: 'classes 9 - 12',
+  },
+];
+
+export const homeMeta = {
+  '001': [
+    {
+      id: '001',
+      name: 'Introduction to Coding',
+      duration: '50 hrs',
+      classes: 25,
+      projects: 2,
+    },
+    {
+      id: '001',
+      name: 'Introduction to Coding',
+      duration: '50 hrs',
+      classes: 25,
+      projects: 2,
+    },
+    {
+      id: '001',
+      name: 'Introduction to Coding',
+      duration: '50 hrs',
+      classes: 25,
+      projects: 2,
+    },
+  ],
+  '002': [
+    {
+      id: '002',
+      name: 'Introduction to Coding',
+      duration: '50 hrs',
+      classes: 25,
+      projects: 2,
+    },
+    {
+      id: '002',
+      name: 'Introduction to Coding',
+      duration: '50 hrs',
+      classes: 25,
+      projects: 2,
+    },
+    {
+      id: '002',
+      name: 'Introduction to Coding',
+      duration: '50 hrs',
+      classes: 25,
+      projects: 2,
+    },
+  ],
+  '003': [
+    {
+      id: '003',
+      name: 'Introduction to Coding',
+      duration: '50 hrs',
+      classes: 25,
+      projects: 2,
+    },
+    {
+      id: '003',
+      name: 'Introduction to Coding',
+      duration: '50 hrs',
+      classes: 25,
+      projects: 2,
+    },
+    {
+      id: '003',
+      name: 'Introduction to Coding',
+      duration: '50 hrs',
+      classes: 25,
+      projects: 2,
+    },
+  ],
+};
