cdabd907fd1bf18c34010f93d71923cadcfcf3f4
MrAzharuddin <42718729+MrAzharuddin@users.noreply.github.com>
2022-11-21 20:06:14 +0530

    1 file modified

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,3 +1,4 @@
+import OwnTab from "../components/OwnTab";
 import Tabs from "../components/Tabs";
 
 export default function Contact() {
@@ -5,7 +6,7 @@ export default function Contact() {
     <div className="min-h-screen flex items-center justify-center">
       <h1>Hello Contact</h1>
       <div>
-        <Tabs/>
+        <OwnTab/>
       </div>
     </div>
   );
