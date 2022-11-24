80b650b03354f4274721f5474bc875ccf792310c
MrAzharuddin <42718729+MrAzharuddin@users.noreply.github.com>
2022-11-24 21:14:21 +0530

    2 files modified

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,13 +1,14 @@
-import FAQs from "../components/FAQs";
+import FAQs from '../components/FAQs';
 
 export default function About() {
   return (
     <div className="min-h-[calc(100vh-12vh)] flex items-center justify-center">
       <h1>Hello About</h1>
-      <div>
-      <FAQs/>
-      </div>
-      
+      <section>
+        <h1 className="text-center">
+          Let’s Empower Ourselves with Today’s Language of Creativity
+        </h1>
+      </section>
     </div>
   );
 }
