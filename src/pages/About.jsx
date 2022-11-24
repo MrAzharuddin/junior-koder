29af74f97c67ca4a7f98d1704293e4f17c5e7e0b
Azharuddin Mohammed <42718729+MrAzharuddin@users.noreply.github.com>
2022-11-24 16:36:41 +0000

    About page added

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,13 +1,99 @@
-import FAQs from '../components/FAQs';
-
 export default function About() {
   return (
-    <div className="min-h-[calc(100vh-12vh)] flex items-center justify-center">
-      <h1>Hello About</h1>
-      <section>
-        <h1 className="text-center">
-          Let’s Empower Ourselves with Today’s Language of Creativity
+    <div className="min-h-[calc(100vh-12vh)] py-6">
+      <section className="space-y-6 ">
+        <h1 className="text-center text-secondary text-2xl font-bold">
+          Let’s Empower Ourselves with Today’s
+          <span className="block text-3xl">Language of Creativity</span>
         </h1>
+        <div className="flex px-8">
+          <div className="bg-primary p-12 rounded-lg space-y-6 text-white max-w-[65vw]">
+            <p className="text-4xl">Our Story</p>
+            <p>
+              In a world where technology is getting smarter, we need to make
+              our kids the smartest! To compete in this world full of
+              competition, we need to be prepared. As Stephen Hawking said, "You
+              want to pursue a career in the 21st Century, basic computer
+              programming is an essential skill to learn". <br /> <br /> We, at
+              Junior Koder, provide one-on-one live and interactive online
+              coding classes for ages 6-18 years. From the comfort of your home,
+              watch your child become a coder with ease. <br /> <br /> The
+              classes are conducted by trained and professional faculty. Let
+              your children be the creators of the new world. Steve Jobs rightly
+              said, "Everyone should learn how to code, it teaches you how to
+              think"
+            </p>
+          </div>
+          <div>
+            <img
+              src="https://storage.googleapis.com/proudcity/mebanenc/uploads/2021/03/placeholder-image.png"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="space-y-6">
+          <h1 className="text-grad text-center">Know Us Better</h1>
+          <div className="flex space-x-24 justify-center">
+            <div className="bg-primary rounded-md text-white w-80 h-80">
+              <p>Our Purpose</p>
+            </div>
+            <div className="bg-primary rounded-md text-white w-80 h-80">
+              <p>Our Mission</p>
+            </div>
+            <div className="bg-primary rounded-md text-white w-80 h-80">
+              <p>Our Vision</p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="p-8 space-y-4">
+        <h1 className="text-grad">Meet Our Instructors</h1>
+        <div className="space-x-8 flex justify-center py-6">
+          {[
+            {
+              name: "Maria Aziz",
+              role: "Software Engineer and Instructor",
+              desc: "Machine Learning Instructor",
+            },
+            {
+              name: "Maria Aziz",
+              role: "Software Engineer and Instructor",
+              desc: "Machine Learning Instructor",
+            },
+            {
+              name: "Maria Aziz",
+              role: "Software Engineer and Instructor",
+              desc: "Machine Learning Instructor",
+            },
+            {
+              name: "Maria Aziz",
+              role: "Software Engineer and Instructor",
+              desc: "Machine Learning Instructor",
+            },
+          ].map((res, index) => {
+            return (
+              <div
+                key={index + res.name}
+                className="flex bg-shape flex-col py-8 px-6 text-white items-center rounded-xl hover:scale-110 transition-all duration-500"
+              >
+                <div className="py-4">
+                  <img
+                    src="https://via.placeholder.com/150"
+                    className="rounded-full"
+                    alt=""
+                  />
+                </div>
+                <div className="text-center space-y-4">
+                  <div>
+                    <p className="text-xl font-semibold">{res.name}</p>
+                    <p className="text-sm">{res.role}</p>
+                  </div>
+                  <p className="text-xl font-semibold">{res.desc}</p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </section>
     </div>
   );
