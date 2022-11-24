2de66d00969121d8b8b584b71589052d1dbfec2d
Azharuddin Mohammed <42718729+MrAzharuddin@users.noreply.github.com>
2022-11-24 18:51:02 +0000

    aos added

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,13 +1,31 @@
+import Aos from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
+
 export default function About() {
+  useEffect(() => {
+    Aos.init({
+      duration: 2000,
+      easing: "ease-in-out",
+      once: false,
+      offset: 120,
+    });
+  }, []);
   return (
     <div className="min-h-[calc(100vh-12vh)] py-6">
       <section className="space-y-6 ">
-        <h1 className="text-center text-secondary text-2xl font-bold">
+        <h1
+          data-aos="fade-down"
+          className="text-center text-secondary text-2xl font-bold"
+        >
           Let’s Empower Ourselves with Today’s
           <span className="block text-3xl">Language of Creativity</span>
         </h1>
         <div className="flex px-8">
-          <div className="bg-primary p-12 rounded-lg space-y-6 text-white max-w-[65vw]">
+          <div
+            data-aos="fade-right"
+            className="bg-primary p-12 rounded-lg space-y-6 text-white max-w-[65vw]"
+          >
             <p className="text-4xl">Our Story</p>
             <p>
               In a world where technology is getting smarter, we need to make
@@ -24,7 +42,7 @@ export default function About() {
               think"
             </p>
           </div>
-          <div>
+          <div data-aos="fade-left">
             <img
               src="https://storage.googleapis.com/proudcity/mebanenc/uploads/2021/03/placeholder-image.png"
               alt=""
@@ -34,13 +52,22 @@ export default function About() {
         <div className="space-y-6">
           <h1 className="text-grad text-center">Know Us Better</h1>
           <div className="flex space-x-24 justify-center">
-            <div className="bg-primary rounded-md text-white w-80 h-80">
+            <div
+              data-aos="zoom-in"
+              className="bg-primary rounded-md text-white w-80 h-80"
+            >
               <p>Our Purpose</p>
             </div>
-            <div className="bg-primary rounded-md text-white w-80 h-80">
+            <div
+              data-aos="zoom-in"
+              className="bg-primary rounded-md text-white w-80 h-80"
+            >
               <p>Our Mission</p>
             </div>
-            <div className="bg-primary rounded-md text-white w-80 h-80">
+            <div
+              data-aos="zoom-in"
+              className="bg-primary rounded-md text-white w-80 h-80"
+            >
               <p>Our Vision</p>
             </div>
           </div>
@@ -98,3 +125,5 @@ export default function About() {
     </div>
   );
 }
+
+
