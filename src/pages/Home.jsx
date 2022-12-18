bcd6fae2ac50b42284cfb179dead43c9540685c5
Azar M <azharuddinmohammed998@gmail.com>
2022-12-18 11:56:20 +0000

    dfd

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -858,7 +858,7 @@ export default function Home() {
             return (
               <div
                 key={index + res.name}
-                className="flex bg-shape min-w-max flex-col py-8 px-6 text-white items-center rounded-xl hover:scale-110 transition-all duration-500"
+                className="flex flex-1 bg-shape min-w-max flex-col py-8 px-6 text-white items-center rounded-xl hover:scale-110 transition-all duration-500"
               >
                 <div className="py-4">
                   <img
