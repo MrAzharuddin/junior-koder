0ba1f659a0ad103511ca3dfbde1a538b278d6ccd
Azar M <azharuddinmohammed998@gmail.com>
2022-12-25 17:38:36 +0000

    hghjh

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -5,35 +5,47 @@ import { images } from "../utils/images";
 function Landing() {
   return (
     <div>
-      <div className="flex gap-4 items-center text-center min-h-[85vh]">
-        <div className="max-w-[35vw] rotate-[24deg] transition-all duration-300 ease-in-out ">
+      <div className="flex gap-4 items-center text-center min-h-[85vh] max-w-screen md:p-0 p-4">
+        <div className="max-w-[35vw] rotate-[24deg] transition-all duration-300 ease-in-out md:block hidden ">
           <img className="w-full" src={images.landing.rocketGif} alt="Rocket" />
         </div>
         <div className="space-y-8 w-full">
-          <div>
-            <h2 className="text-grad2 drop-shadow-xl text-5xl">
+          <div className="space-y-4">
+            <h2 className="text-grad2 drop-shadow-xl md:text-5xl text-3xl">
               Code Your Future With Junior Koder
             </h2>
-            <p className="text-grad2 text-2xl">
+            <p className="text-grad2 md:text-2xl text-xl">
               A place where your kid is taught to love coding
             </p>
           </div>
-          <div className="flex justify-center font-bold leading-5">
+          <div className="flex justify-center font-bold leading-5 md:text-base text-sm">
             <div className="flex text-primary gap-8">
-              <div className="flex text-left items-center">
-                <img src={images.landing.calender} alt="calender" />
+              <div className="flex flex-wrap md:text-left md:items-center mx-auto">
+                <img
+                  className="md:p-0 p-2"
+                  src={images.landing.calender}
+                  alt="calender"
+                />
                 <p>
                   Easy class <br /> scheduling
                 </p>
               </div>
-              <div className="flex text-left items-center">
-                <img src={images.landing.bilingual} alt="bilingual" />
+              <div className="flex flex-wrap md:text-left md:items-center mx-auto">
+                <img
+                  className="md:p-0 p-2"
+                  src={images.landing.bilingual}
+                  alt="bilingual"
+                />
                 <p>
                   Billingual <br /> Tutor
                 </p>
               </div>
-              <div className="flex text-left items-center">
-                <img src={images.landing.disk} alt="disk" />
+              <div className="flex flex-wrap md:text-left md:items-center mx-auto">
+                <img
+                  className="md:p-0 p-2"
+                  src={images.landing.disk}
+                  alt="disk"
+                />
                 <p>
                   Recorded <br /> Classes
                 </p>
@@ -42,7 +54,10 @@ function Landing() {
           </div>
           <div>
             <button className="px-8 py-2 rounded-full bg-primary shadow-lg">
-              <Link className="text-white font-semibold" to="/">
+              <Link
+                className="text-white font-semibold md:text-base text-sm"
+                to="/"
+              >
                 Book Your Free Trial Now
               </Link>
             </button>
