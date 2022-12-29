14e1541f2f1163054a4edd951a9647de89d3cd24
Azar M <azharuddinmohammed998@gmail.com>
2022-12-29 19:03:45 +0000

    Update Test.jsx

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -22,9 +22,15 @@ function Test() {
           "https://picsum.photos/id/1/1200/600",
           "https://picsum.photos/id/2/1200/600",
           "https://picsum.photos/id/3/1200/600",
-          "https://picsum.photos/id/1/1200/600",
-          "https://picsum.photos/id/2/1200/600",
-          "https://picsum.photos/id/3/1200/600",
+          "https://picsum.photos/id/4/1200/600",
+          "https://picsum.photos/id/5/1200/600",
+          "https://picsum.photos/id/6/1200/600",
+          "https://picsum.photos/id/7/1200/600",
+          "https://picsum.photos/id/8/1200/600",
+          "https://picsum.photos/id/9/1200/600",
+          "https://picsum.photos/id/10/1200/600",
+          "https://picsum.photos/id/11/1200/600",
+          "https://picsum.photos/id/16/1200/600",
         ]}
         intervalTime={5000}
       />
