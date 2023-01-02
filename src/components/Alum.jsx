dc6de838d986fc8dfbcda492b228532efafb167d
MrAzharuddin <azharuddinmohammed998@gmail.com>
2023-01-02 17:53:32 +0530

    sajdlkdf

--- src/components/Alum.jsx
+++ src/components/Alum.jsx
@@ -4,7 +4,7 @@ function Alum() {
   return (
     <div className='md:p-8'>
         <div className='space-y-6'>
-            <h1 className='text-grad font-bold md:text-4xl text-2xl md:px-12 px-6 text-center'>Our courses curated by alums and teaching experts from the top universities and tech-gaints</h1>
+            <h1 className='text-grad font-bold md:text-4xl text-2xl md:px-12 text-center'>Our courses curated by alums and teaching experts from the top universities and tech-gaints</h1>
             <hr className='bg-shape h-[3px] w-1/2 mx-auto rounded-lg'/>
             <div className='flex flex-wrap justify-center lg:w-2/3 mx-auto md:gap-8 gap-4'>
                 <div className='flex-1  xl:max-w-[20%] xl:min-w-[18%] lg:min-w-[15%] lg:max-w-[18%] md:min-w-[15%] md:max-w-[18%] max-w-[26%] min-w-[25%] '><img src="https://firebasestorage.googleapis.com/v0/b/juniorkoder-website.appspot.com/o/google.png?alt=media&token=d64d6025-7416-4a14-b533-37aca10b0e81" alt="" /></div>
