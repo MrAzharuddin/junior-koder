8597f12c72e0065f6ee69d00654d3bf924e5008a
Azar M <azharuddinmohammed998@gmail.com>
2023-01-06 14:44:39 +0000

    Update 2 files

--- src/components/Form/FormVerify.jsx
+++ src/components/Form/FormVerify.jsx
@@ -2,26 +2,27 @@ import React, { useState, useEffect } from "react";
 import BasicVerify from "./BasicVerify";
 import MobileVerify from "./MobileVerify";
 import OTPVerify from "./OTPVerify";
+import SuccessForm from "./SuccessForm";
 
 function FormVerify() {
   const [route, setRoute] = useState(1);
   useEffect(() => {
     console.log(route);
-  }, [route])
+  }, [route]);
   function getComponents(route) {
-  switch (route) {
-    case 1:
-      return <MobileVerify routeUpdate={()=>setRoute(2)}/>
-    case 2:
-      return <OTPVerify routeUpdate={()=>setRoute(3)}/>
-    case 3:
-      return <BasicVerify routeUpdate={()=>setRoute(4)}/>
-    case 4:
-      return <div>Hello world</div>
-    default:
-      return <MobileVerify routeUpdate={()=>setRoute(2)}/>
+    switch (route) {
+      case 1:
+        return <MobileVerify routeUpdate={() => setRoute(2)} />;
+      case 2:
+        return <OTPVerify routeUpdate={() => setRoute(3)} />;
+      case 3:
+        return <BasicVerify routeUpdate={() => setRoute(4)} />;
+      case 4:
+        return <SuccessForm />;
+      default:
+        return <MobileVerify routeUpdate={() => setRoute(2)} />;
+    }
   }
-}
   return (
     <div className="text-white form-grad min-h-screen p-8">
       <div>
@@ -38,9 +39,7 @@ function FormVerify() {
             <img src="https://picsum.photos/id/4/1200/600" alt="" />
           </div>
         </div>
-        <div>
-          {getComponents(route)}
-        </div>
+        <div>{getComponents(route)}</div>
       </div>
     </div>
   );
